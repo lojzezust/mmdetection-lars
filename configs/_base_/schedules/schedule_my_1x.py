aa3dda6f8a1561e2fbcefe9c47c45c526f90e22d
Lojze Žust <lojze.zust@fri.uni-lj.si>
2023-05-21 20:21:55 +0000

    Bump default schedule

--- configs/_base_/schedules/schedule_my_1x.py
+++ configs/_base_/schedules/schedule_my_1x.py
@@ -3,14 +3,14 @@
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 
-max_iters = 5000 # ~ 50 epoh
+max_iters = 50000 # ~ 50 epoh
 
 # learning policy
 lr_config = dict(
     policy='step',
     gamma=0.1,
     by_epoch=False,
-    step=[4000, 4700],
+    step=[40000, 47000],
     warmup='linear',
     warmup_by_epoch=False,
     warmup_ratio=0.001,
